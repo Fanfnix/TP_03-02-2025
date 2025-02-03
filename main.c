cdf7aee3c3febee96056dc5307d81e4e66e661f4
Fanfnix <valentin@familledenis.net>
2025-02-03 10:00:33 +0100

    Test_categorize_age

--- main.c
+++ main.c
@@ -6,17 +6,69 @@
 
 #define mu_run_test(test) do {char * message = test(); test_run++; if(message) return message;} while (0)
 
-int carre(a) {
-    return a * a;
+int test_run = 0;
+
+static const char * categorize_age(int age) {
+    if (age < 0) {
+        return "Invalid age";
+    }
+    else if (age < 13) {
+        return "Child";
+    }
+    else if (age < 20) {
+        return "Teenager";
+    }
+    else if (age < 65) {
+        return "Adult";
+    }
+    else {
+        return "Senior";
+    }
 }
 
-char * test_carre() {
-    mu_assert("ERR : carre(3) != 9\n", carre(3) == 9);
-    mu_assert("ERR : carre(-1) != 1\n", carre(-1) == 1);
-    mu_assert("ERR : carre(-5) != 1\n", carre(-5) == 25);
+static char * test_categorize_age_neg() {
+    mu_assert("ERR : categorize(-1) != 'Invalid age'\n", categorize_age(-1) == "Invalid age");
+    return 0;
+}
+
+static char * test_categorize_age_child() {
+    mu_assert("ERR : categorize(10) != 'Child'\n", categorize_age(10) == "Child");
+    return 0;
+}
+
+static char * test_categorize_age_teen() {
+    mu_assert("ERR : categorize(18) != 'Teenager'\n", categorize_age(18) == "Teenager");
+    return 0;
+}
+
+static char * test_categorize_age_adult() {
+    mu_assert("ERR : categorize(40) != 'Adult'\n", categorize_age(40) == "Adult");
+    return 0;
+}
+
+static char * test_categorize_age_senior() {
+    mu_assert("ERR : categorize(90) != 'Senior'\n", categorize_age(90) == "Senior");
+    return 0;
+}
+
+static char * all_test() {
+    mu_run_test(test_categorize_age_neg);
+    mu_run_test(test_categorize_age_child);
+    mu_run_test(test_categorize_age_teen);
+    mu_run_test(test_categorize_age_adult);
+    mu_run_test(test_categorize_age_senior);
+    return 0;
 }
 
 int main() {
+    char * result = all_test();
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("All_test passé\n");
+    }
+    printf("TEST_RUN = %d\n", test_run);
 
     return 0;
 }
